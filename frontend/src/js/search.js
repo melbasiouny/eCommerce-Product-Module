932c0d2f803888b7ef1c764853b6a14156849433
melbasiouny <melbasiouny@pm.me>
2023-11-18 15:19:20 -0500

    Fix product hover style to match the main product view page

--- frontend/src/js/search.js
+++ frontend/src/js/search.js
@@ -32,14 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 products.forEach(product => {
                     const is_low_stock = product.stock <= 10;
                     const product_html =
-                        `<div class="product rounded" style="margin-bottom: 40px; margin-left: 10px; margin-right: 10px; margin-top: 10px; width: 180px; height: auto; position: relative; overflow: hidden;" data-product-id="${product.pid}" onmouseover="this.style.boxShadow='inset 0 0 8px 1px rgba(255, 178, 82, 0.6), 0 0 16px 4px rgba(255, 200, 82, 0.3)'; this.style.transform='scale(1.05)';" onmouseout="this.style.boxShadow='none'; this.style.transform='scale(1)';">
+                        `<div class="product rounded" style="margin-bottom: 40px; margin-left: 10px; margin-right: 10px; margin-top: 10px; width: 180px; height: auto; position: relative; overflow: hidden;" data-product-id="${product.pid}">
                             <img class="rounded" style="padding: 12px; object-fit: contain; margin-bottom: 0px;" src="${product.image}" width="180" height="120">
                             <div class="status" style="position: absolute; top: 0px; left: 4px;">    
-                                ${is_product_selling_fast(product) ? '<p style="color: black;"><i class="bi bi-fire" ></i></p>' : ''}
+                                ${is_product_selling_fast(product) ? '<p style="color: black;"><i class="bi bi-fire"></i></p>' : ''}
                             </div>
-                            <div class="rating" style="position: absolute; top: 0px; right: 6px;">
+                            <div class="rating" style="position: absolute; top: 0px; right: 4px;">
                                 <span style="color: black; font-weight: bold; font-size: 14px;">${product.rating}</span>
-                            <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
                             </div>
                             <div class="text-uppercase fw-bold product-name" style="padding-left: 4px; padding-right: 4px; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; white-space: normal; text-overflow: ellipsis; overflow-wrap: break-word; z-index: 2;">
                                 ${is_low_stock ? '<i class="bi bi-exclamation-circle-fill"></i>' : ''}
@@ -48,7 +48,22 @@ document.addEventListener("DOMContentLoaded", function () {
                             <div class="product-price" style="padding-left: 4px; padding-right: 4px; z-index: 2;">C$ ${product.price}</div>
                         </div>`;
 
-                    product_container.innerHTML += product_html;
+                    const productElement = document.createElement('div');
+                    productElement.innerHTML = product_html;
+
+
+                    productElement.addEventListener('mouseenter', function () {
+                        this.style.boxShadow = 'inset 0 0 8px 1px rgba(255, 178, 82, 0.6), 0 0 16px 4px rgba(255, 200, 82, 0.3)';
+                        this.style.borderRadius = '16px';
+                        this.style.transform = 'scale(1.05)';
+                    });
+
+                    productElement.addEventListener('mouseleave', function () {
+                        this.style.boxShadow = 'none';
+                        this.style.transform = 'scale(1)';
+                    });
+
+                    product_container.appendChild(productElement);
                 });
 
                 if (products.length > 0) {
