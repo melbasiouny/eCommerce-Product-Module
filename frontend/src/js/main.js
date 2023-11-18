de0b8e28b17b79bc7831aab7f3d83fc22cb31481
melbasiouny <melbasiouny@pm.me>
2023-11-18 15:17:57 -0500

    Send analytical data to the analysis module on product hover

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -38,14 +38,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 products.forEach(product => {
                     const is_low_stock = product.stock <= 10;
                     const product_html =
-                        `<div class="product rounded" style="margin-bottom: 40px; margin-left: 10px; margin-right: 10px; margin-top: 10px; width: 180px; height: auto; position: relative; overflow: hidden;" data-product-id="${product.pid}" onmouseover="this.style.boxShadow='inset 0 0 8px 1px rgba(255, 178, 82, 0.6), 0 0 16px 4px rgba(255, 200, 82, 0.3)'; this.style.transform='scale(1.05)';" onmouseout="this.style.boxShadow='none'; this.style.transform='scale(1)';">
+                        `<div class="product rounded" style="margin-bottom: 40px; margin-left: 10px; margin-right: 10px; margin-top: 10px; width: 180px; height: auto; position: relative; overflow: hidden;" data-product-id="${product.pid}">
                             <img class="rounded" style="padding: 12px; object-fit: contain; margin-bottom: 0px;" src="${product.image}" width="180" height="120">
                             <div class="status" style="position: absolute; top: 0px; left: 4px;">    
-                                ${is_product_selling_fast(product) ? '<p style="color: black;"><i class="bi bi-fire" ></i></p>' : ''}
+                                ${is_product_selling_fast(product) ? '<p style="color: black;"><i class="bi bi-fire"></i></p>' : ''}
                             </div>
                             <div class="rating" style="position: absolute; top: 0px; right: 4px;">
                                 <span style="color: black; font-weight: bold; font-size: 14px;">${product.rating}</span>
-                            <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
                             </div>
                             <div class="text-uppercase fw-bold product-name" style="padding-left: 4px; padding-right: 4px; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; white-space: normal; text-overflow: ellipsis; overflow-wrap: break-word; z-index: 2;">
                                 ${is_low_stock ? '<i class="bi bi-exclamation-circle-fill"></i>' : ''}
@@ -54,7 +54,27 @@ document.addEventListener("DOMContentLoaded", function () {
                             <div class="product-price" style="padding-left: 4px; padding-right: 4px; z-index: 2;">C$ ${product.price}</div>
                         </div>`;
 
-                    product_container.innerHTML += product_html;
+                    const productElement = document.createElement('div');
+                    var hover_start_duration = null;
+                    productElement.innerHTML = product_html;
+
+
+                    productElement.addEventListener('mouseenter', function () {
+                        this.style.boxShadow = 'inset 0 0 8px 1px rgba(255, 178, 82, 0.6), 0 0 16px 4px rgba(255, 200, 82, 0.3)';
+                        this.style.borderRadius = '16px';
+                        this.style.transform = 'scale(1.05)';
+                        hover_start_duration = Date.now();
+                    });
+
+                    productElement.addEventListener('mouseleave', function () {
+                        this.style.boxShadow = 'none';
+                        this.style.transform = 'scale(1)';
+
+                        const hover_duration = Date.now() - hover_start_duration;
+                        analytics_on_mouse_hover(product.pid, hover_duration)
+                    });
+
+                    product_container.appendChild(productElement);
                 });
 
                 page_number.textContent = current_page.toString();
@@ -86,6 +106,32 @@ document.addEventListener("DOMContentLoaded", function () {
             product.clicks >= CLICKS_THRESHOLD
         );
     }
+
+    async function analytics_on_mouse_hover(product_id, hover_duration) {
+        try {
+            const response = await fetch(
+                `https://analysisreportingdatabasemodulegroup1.azurewebsites.net/Group1/DatabaseController/POST/minhnguyen/Connhenbeo1/group1/0`,
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        "UserView_ID": Math.floor(Math.random() * 1000),
+                        "User_ID": "69",
+                        "Product_ID": product_id,
+                        "Time_Count": hover_duration,
+                        "Date_Access": `${new Date().toISOString().slice(0, 19)}`
+                    })
+                }
+            );
+
+            if (!response.ok) {
+                console.error('Error, unable to send product hover data to analysis module. Fix your shit man:', product_id);
+            }
+        } catch (error) {
+            console.error('Error while sending POST request:', error);
+        }
+    }
+
 
     function go_to_product(product_id) {
         const request_options = {
